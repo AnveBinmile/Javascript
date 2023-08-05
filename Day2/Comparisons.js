eb19a2e478adef58144a2e7c96430dd994f98646
anvesha <anvesha@binmile.com>
2023-08-05 18:03:02 +0530

    changes done

--- Day2/Comparisons.js
+++ Day2/Comparisons.js
@@ -1,80 +1,72 @@
-let a=2;
-let b=3;
-let c=2;
+let a = 2;
+let b = 3;
+let c = 2;
 
-let s="2";
+let s = "2";
 
-console.log(a==b); //false
-console.log(a>b); //false
-console.log(a<b) //true
-console.log(a==c); //true
-console.log(a==s); //true
-console.log(a===s); //false;
-console.log(a!==b);//true
-
+console.log(a == b); //false
+console.log(a > b); //false
+console.log(a < b); //true
+console.log(a == c); //true
+console.log(a == s); //true
+console.log(a === s); //false;
+console.log(a !== b); //true
 
 let number = 3;
-if(number<3){
-    console.log('Less than 3')
+if (number < 3) {
+  console.log("Less than 3");
+} else if (number == 3) {
+  console.log("Equal to  3");
+} else {
+  console.log("Greater than 3");
 }
-else if(number==3){
-    console.log('Equal to  3')
-}
-else{
-    console.log('Greater than 3')
-}
-
-
 
 // (number>=3)?((number>3)?(console.log('Number greater than 3')):('Number equal to 3')):console.log('Number is less than 3');
 
-
-
 //NESTED TERNARY
-let txt = number>=3?((number>3)?'Greater than three':'Equal to three'):'Less than three';
+let txt =
+  number >= 3
+    ? number > 3
+      ? "Greater than three"
+      : "Equal to three"
+    : "Less than three";
 
 console.log(txt);
 
-(number==3)?console.log('Number is equal to 3'):console.log('Number is not equal  to three')
+number == 3
+  ? console.log("Number is equal to 3 ")
+  : console.log("Number is not equal  to three");
 
+let n = 10;
 
-
-
-let n=10;
-
-for(;n>=5;n--){
-    console.log('for ',n);
+for (; n >= 5; n--) {
+  console.log("for ", n);
 }
 
-while(n>0){
-    console.log('while ' ,n);
-    n--;
+while (n > 0) {
+  console.log("while ", n);
+  n--;
 }
 
 do {
-    console.log('do while ',n);
-}
-while(n>0)
+  console.log("do while ", n);
+} while (n > 0);
 
-
-
-const arr = ['Mango','Apple','Strawberry','Cherry'];
+const arr = ["Mango", "Apple", "Strawberry", "Cherry"];
 
 //FOR OF
-for(const item of arr){
-    console.log('for..of  ',item);
+for (const item of arr) {
+  console.log("for..of  ", item);
 }
 
 //FOR IN
-for(const item in arr){
-    console.log(item,' for-in  ',arr[item]);
+for (const item in arr) {
+  console.log(item, " for-in  ", arr[item]);
 }
 
-
 //FOR EACH
-arr.forEach((val,index)=>{
-    val+= '_fruit';
-    arr[index]+='__fruits ';
-    console.log(val,'   ->  ',arr[index]);
-})
-
+arr.forEach((val, index) => {
+  val += "_fruit";
+  arr[index] += "__fruits ";
+  console.log(val, "   ->  ", arr[index]);
+});
---
+++ Day3/Assignment.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a+b;
+}
+
+ (a,b)=>{return a+b;}
+
+ console.log( (a,b)=>{return a+b;}
+ )
+
+let m = new Map();
+
+m.set('Strawberry','40');
+m.set('Mangoes','90');
+m.set('Apples',100);
+
+console.log(m);
+
+const arr=[1,2,3,4,5];
+
+console.log(arr);
+
+arr.length=1;
+
+console.log(arr);
+
+console.log(3+2+"7")
+
+// arr.length=2
+
+console.log(this);
