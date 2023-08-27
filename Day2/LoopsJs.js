9cb5a4cd9a5420afc664b860172e51c7eb66bdfc
anvesha <anvesha@binmile.com>
2023-08-28 01:28:33 +0530

    loops

---
+++ Day2/LoopsJs.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration:", i);
+}
+
+let count = 0;
+while (count < 5) {
+  console.log("Count:", count);
+  count++;
+}
+
+let x = 0;
+do {
+  console.log("x:", x);
+  x++;
+} while (x < 5);
